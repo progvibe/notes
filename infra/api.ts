a126fac71d5914bce1b1a01b77be6fdaf4f9e6ad
Austin <progvibe@fastmail.com>
2024-09-18 09:51:41 -0400

    front end

--- infra/api.ts
+++ infra/api.ts
@@ -1,14 +1,15 @@
 import { table, secret } from "./storage";
 
 export const api = new sst.aws.ApiGatewayV2("Api", {
+  cors: true,
   transform: {
     route: {
       handler: {
         link: [table, secret],
       },
       args: {
-        auth: { iam: true }
-      }
+        auth: { iam: true },
+      },
     },
   },
 });
